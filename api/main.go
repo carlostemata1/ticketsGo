0082b792b376b794bc28ba0fd3ca18bd1d611a59
carlostemata1 <1carlitosandres@gmail.com>
2021-08-10 04:59:09 -0500

    ParaEnviar

--- api/main.go
+++ api/main.go
@@ -24,7 +24,10 @@ func conexionBD() (conexion *sql.DB) {
 var plantillas = template.Must(template.ParseGlob("plantillas/*"))
 
 func main() {
+	http.HandleFunc("/", ListarTickets)
 	http.HandleFunc("/listarTickets", ListarTickets)
+	http.HandleFunc("/borrados", Borrados)
+	http.HandleFunc("/recuperar", Recuperar)
 	http.HandleFunc("/crearTicket", CrearTicket)
 	http.HandleFunc("/insertarTicket", InsertarTicket)
 	http.HandleFunc("/borrar", Borrar)
@@ -157,7 +160,6 @@ func Editar(w http.ResponseWriter, r *http.Request) {
 		ticket.FechaActualizacion = fechaActualizacion
 		ticket.Estatus = estatus
 	}
-	fmt.Print(ticket)
 
 	plantillas.ExecuteTemplate(w, "editar", ticket)
 
@@ -183,4 +185,71 @@ func Actualizar(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-//---------------------------------------fin del medicamento --------------------------------------
+func Borrados(w http.ResponseWriter, r *http.Request) {
+	conexionEstablecida := conexionBD()
+	obtenerRegistros, err := conexionEstablecida.Query("SELECT * FROM ticketBorrados")
+
+	if err != nil {
+		panic(err.Error())
+	}
+	ticket := Ticket{}
+	arregloTicket := []Ticket{}
+
+	for obtenerRegistros.Next() {
+		var id int
+		var usuario string
+		var fechaCreacion string
+		var fechaActualizacion string
+		var estatus bool
+		err = obtenerRegistros.Scan(&id, &usuario, &fechaCreacion, &fechaActualizacion, &estatus)
+		if err != nil {
+			panic(err.Error())
+		}
+		ticket.Id = id
+		ticket.Usuario = usuario
+		ticket.FechaCreacion = fechaCreacion
+		ticket.FechaActualizacion = fechaActualizacion
+		ticket.Estatus = estatus
+
+		arregloTicket = append(arregloTicket, ticket)
+	}
+	plantillas.ExecuteTemplate(w, "borrados", arregloTicket)
+}
+
+func Recuperar(w http.ResponseWriter, r *http.Request) {
+	idTicket := r.URL.Query().Get("id")
+	conexionEstablecida := conexionBD()
+
+	obtenerRegistro, err := conexionEstablecida.Query("SELECT * FROM ticketBorrados WHERE id=?", idTicket)
+	ticket := Ticket{}
+	for obtenerRegistro.Next() {
+		var id int
+		var usuario string
+		var fechaCreacion string
+		var fechaActualizacion string
+		var estatus bool
+		err = obtenerRegistro.Scan(&id, &usuario, &fechaCreacion, &fechaActualizacion, &estatus)
+		if err != nil {
+			panic(err.Error())
+		}
+		ticket.Id = id
+		ticket.Usuario = usuario
+		ticket.FechaCreacion = fechaCreacion
+		ticket.FechaActualizacion = fechaActualizacion
+		ticket.Estatus = estatus
+		insertarRegistro, err := conexionEstablecida.Prepare("INSERT INTO tickets (usuario, fechaCreacion, fechaActualizacion,estatus) VALUES (?,?,?,?)")
+		if err != nil {
+			panic(err.Error())
+		}
+		insertarRegistro.Exec(usuario, fechaCreacion, fechaActualizacion, estatus)
+	}
+
+	borrarTicket, err := conexionEstablecida.Prepare("DELETE FROM ticketBorrados WHERE id=?")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	borrarTicket.Exec(idTicket)
+	http.Redirect(w, r, "/borrados", 301)
+
+}
